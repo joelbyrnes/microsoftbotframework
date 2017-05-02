1086c6a9bb9e528781b874b94894460b16ac0567
Matthew Brown <mbrown1508@outlook.com>
2017-05-02 21:46:40 +1000

    updated heroku.py to use new class

--- heroku.py
+++ heroku.py
@@ -12,8 +12,8 @@ import os
 if __name__ == "__main__":
     import microsoftbotframework.runcelery
 
-    bot = MsBot(port=int(os.environ['PORT']), debug=True)
+    bot = MsBot(__name__, port=int(os.environ['PORT']))
     bot.add_process(respond_to_conversation_update)
     bot.add_process(echo_response_async)
     bot.add_process(echo_response)
-    bot.run()
+    bot.run(debug=True)
