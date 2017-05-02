82fa65b483479a8fcf47fc51c8ec976582d51ec2
Matthew Brown <mbrown1508@outlook.com>
2017-05-02 21:35:10 +1000

    Refactored msbot to extend Flask

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import microsoftbotframework.runcelery
 from microsoftbotframework import MsBot
 from tasks import *
 
-
-bot = MsBot(verify_jwt_signature=False, debug=True)
+bot = MsBot(__name__, verify_jwt_signature=False)
 bot.add_process(respond_to_conversation_update)
 bot.add_process(echo_response_async) # Only uncomment if a celery backend is configured
 bot.add_process(echo_response)
-bot.run()
+bot.run(debug=True)
--- microsoftbotframework/msbot.py
+++ microsoftbotframework/msbot.py
@@ -12,17 +12,21 @@ try:
 except ImportError:
     pass
 
-class MsBot:
-    def __init__(self, host=None, port=None, debug=None, app_client_id=None, redis_uri=None, verify_jwt_signature=None):
-        self.app = Flask(__name__)
+
+class MsBot(Flask):
+    def __init__(self, import_name, *args, **kwargs):
+        app_client_id = kwargs.pop('app_client_id', None)
+        reset_url = kwargs.pop('reset_url', None)
+        verify_jwt_signature = kwargs.pop('verify_jwt_signature', None)
+
+        super(MsBot, self).__init__(import_name, *args, **kwargs)
+
+        self.add_url_rule('/api/messages', methods=['POST'], view_func=self._message_post)
 
         self.processes = []
         config = Config()
-        self.host = config.get_config(host, 'HOST', root='flask')
-        self.port = config.get_config(port, 'PORT', root='flask')
-        self.debug = config.get_config(debug, 'DEBUG', root='flask')
         self.app_client_id = config.get_config(app_client_id, 'APP_CLIENT_ID')
-        self.redis_uri = config.get_config(redis_uri, 'URI', root='redis')
+        self.redis_uri = config.get_config(reset_url, 'URI', root='redis')
         self.redis = None
 
         try:
@@ -34,42 +38,38 @@ class MsBot:
 
         self.cache_certs = True
         if self.redis_uri is None:
-            self.app.logger.info('The \'REDIS_URI\' has not been set. Disabling certificate caching.')
+            self.logger.info('The \'REDIS_URI\' has not been set. Disabling certificate caching.')
             self.cache_certs = False
 
         self.redis_config = config.get_section_config('redis')
 
-        @self.app.route('/api/messages', methods=['POST'])
-        def message_post():
-            if self.verify_jwt_signature:
-                valid_token = self._verify_token(request)
-            else:
-                valid_token = True
-
-            if valid_token:
-                json_message = request.get_json()
-
-                json_headers = {}
-                for key, value in request.headers:
-                    json_headers[key] = value
-
-                self.app.logger.info('message.headers: {}'.format(json.dumps(json_headers)))
-                self.app.logger.info('message.body: {}'.format(json.dumps(json_message)))
-
-                for process in self.processes:
-                    if isinstance(process, PromiseProxy):
-                        self.app.logger.info('Processing task {} asynchronously.'.format(type(process).__name__))
-                        process.delay(json_message)
-                    elif callable(process):
-                        self.app.logger.info('Processing task {} synchronously.'.format(process.__name__))
-                        process(json_message)
-                return "Success"
-
     def add_process(self, process):
         self.processes.append(process)
 
-    def run(self):
-        self.app.run(host=self.host, port=self.port, debug=self.debug)
+    def _message_post(self):
+        if self.verify_jwt_signature:
+            valid_token = self._verify_token(request)
+        else:
+            valid_token = True
+
+        if valid_token:
+            json_message = request.get_json()
+
+            json_headers = {}
+            for key, value in request.headers:
+                json_headers[key] = value
+
+            self.logger.info('message.headers: {}'.format(json.dumps(json_headers)))
+            self.logger.info('message.body: {}'.format(json.dumps(json_message)))
+
+            for process in self.processes:
+                if isinstance(process, PromiseProxy):
+                    self.logger.info('Processing task {} asynchronously.'.format(type(process).__name__))
+                    process.delay(json_message)
+                elif callable(process):
+                    self.logger.info('Processing task {} synchronously.'.format(process.__name__))
+                    process(json_message)
+            return "Success"
 
     def _verify_token(self, request, forced_refresh=False):
         authorization_header = request.headers['Authorization']
@@ -85,7 +85,7 @@ class MsBot:
 
         # 1. The token was sent in the HTTP Authorization header with 'Bearer' scheme
         if authorization_scheme != "Bearer":
-            self.app.logger.warning('The token was not sent in the http authorisation header with the Bearer scheme.')
+            self.logger.warning('The token was not sent in the http authorisation header with the Bearer scheme.')
             return False
 
         # 2. The token is valid JSON that conforms to the JWT standard (see references)
@@ -103,25 +103,25 @@ class MsBot:
                 try:
                     decoded_jwt = jwt.decode(token, public_key, algorithms=['RS256'], audience=self.app_client_id)
                 except jwt.exceptions.InvalidTokenError as e:
-                    self.app.logger.warning('{}'.format(e))
+                    self.logger.warning('{}'.format(e))
                     return False
 
         if decoded_jwt is None:
             if self.cache_certs and not forced_refresh:
                 # Force cache refresh
-                self.app.logger.warning('Forcing cache refresh as no valid certificate was found.')
+                self.logger.warning('Forcing cache refresh as no valid certificate was found.')
                 self._get_remote_certificates()
                 return self._verify_token(request, forced_refresh=True)
 
-            self.app.logger.warning('No valid certificate was found to verify JWT')
+            self.logger.warning('No valid certificate was found to verify JWT')
             return False
 
         # 3. The token contains an issuer claim with value of https://api.botframework.com
         if decoded_jwt['iss'] != 'https://api.botframework.com':
-            self.app.logger.warning('The token issuer claim had the incorrect value of {}'.format(decoded_jwt['iss']))
+            self.logger.warning('The token issuer claim had the incorrect value of {}'.format(decoded_jwt['iss']))
             return False
 
-        self.app.logger.info('Token was validated - {}'.format(json.dumps(decoded_jwt)))
+        self.logger.info('Token was validated - {}'.format(json.dumps(decoded_jwt)))
         return decoded_jwt
 
     def _get_remote_certificates(self):
@@ -144,7 +144,7 @@ class MsBot:
         self.redis.set("valid_certificates", json.dumps(valid_certificates))
         self.redis.set("certificates_expire_at", expires_at_string)
 
-        self.app.logger.info('Certificates stored')
+        self.logger.info('Certificates stored')
 
     @staticmethod
     def _has_certificate_expired(expires_at):
@@ -161,8 +161,8 @@ class MsBot:
 
         if valid_certificates is None or certificates_expire_at is None or \
                 self._has_certificate_expired(certificates_expire_at.decode('UTF-8')):
-            self.app.logger.info('Getting remote certificates')
+            self.logger.info('Getting remote certificates')
             return self._get_remote_certificates()
         else:
-            self.app.logger.info('Got stored certificates')
+            self.logger.info('Got stored certificates')
             return json.loads(valid_certificates.decode('UTF-8'))
